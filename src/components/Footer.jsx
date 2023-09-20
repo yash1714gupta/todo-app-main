dc8cfa06b82158220f6fc71357e2e674af39d70d
yash1714gupta <yashg1714@gmail.com>
2023-09-20 14:08:17 +0530

    project completed

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className="attribution">
+          <p>
+            Challenge by{' '}
+            <a href="https://www.frontendmentor.io">Frontend Mentor</a>
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+};
